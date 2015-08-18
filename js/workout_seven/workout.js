fa40a163f4f9fbca167dd48e5b5c15de8a7f6d7e
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2015-08-18 14:31:48 +0200

    2015-08-18 new functions

--- js/workout_seven/workout.js
+++ js/workout_seven/workout.js
@@ -1,28 +1,3 @@
-// definiujemy klasę modelu za pomocą funkcji konstruktora
-// model pojedynczego ćwieczenia
-function Excercise(args) {
-    this.name = args.name;
-    this.title = args.title;
-    this.description = args.description;
-    this.image = args.image;
-    this.related = {};
-    this.related.videos = args.videos;
-    this.nameSound = args.nameSound;
-    this.procedure = args.procedure;
-}
-
-// model zestawu ćwiczeń
-function WorkoutPlan(args) {
-
-    // tablica obiektów w formacie
-    // {exercise : new Exercise({}), duration : 30}
-    this.exercises = [];
-
-    this.name = args.name;
-    this.title = args.title;
-    this.restBetweenExercises = args.restBetweenExercises;
-}
-
 // 2 unikamy błędów powstałych po minimalizacji kodu
 /*
 function WorkoutController($scope) {
@@ -47,4 +22,60 @@ angular.module('workout_seven').controller(['$scope', function($scope) {
 // 1
 angular.module('workout_seven').controller('WorkoutController', function($scope) {
 
+    var restExercise;
+    var WorkoutPlan;
+
+    // definiujemy klasę modelu za pomocą funkcji konstruktora
+    // model pojedynczego ćwieczenia
+    function Exercise(args) {
+        this.name = args.name;
+        this.title = args.title;
+        this.description = args.description;
+        this.image = args.image;
+        this.related = {};
+        this.related.videos = args.videos;
+        this.nameSound = args.nameSound;
+        this.procedure = args.procedure;
+    }
+
+    // model zestawu ćwiczeń
+    function WorkoutPlan(args) {
+
+        // tablica obiektów w formacie
+        // {exercise : new Exercise({}), duration : 30}
+        this.exercises = [];
+
+        this.name = args.name;
+        this.title = args.title;
+        this.restBetweenExercises = args.restBetweenExercises;
+    }
+
+    var startExercise = function() {
+        // Javascript: Understanding the Weird Parts
+    };
+
+    var createWorkout = function() {
+
+    };
+
+    var startWorkout = function() {
+        workoutPlan = createWorkout();
+        restExercise = {
+            details : new Exercise({
+                name : "rest",
+                title : "Relax!",
+                description : "Relax a bit!",
+                image : "img/rest.png"
+            }),
+            duration : workoutPlan.restBetweenExercises
+        };
+
+        startExercise(workoutPlan.exercises.shift());
+    };
+
+    var init = function() {
+        startWorkout();
+    };
+
+    init();
 });
