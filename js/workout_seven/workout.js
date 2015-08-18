7c956e4dc3a54dbdd7046b8f26ef70e27fd6613f
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2015-08-18 05:17:02 +0200

    2015-08-18 DI

--- js/workout_seven/workout.js
+++ js/workout_seven/workout.js
@@ -23,6 +23,28 @@ function WorkoutPlan(args) {
     this.restBetweenExercises = args.restBetweenExercises;
 }
 
+// 2 unikamy błędów powstałych po minimalizacji kodu
+/*
+function WorkoutController($scope) {
+
+}
+
+// static property $inject, wskazujemy na tablicę, która zawiera anotacje w postaci string
+WorkoutController['$inject'] = ['$scope'];
+
+angular.module('workout_seven').controller('WorkoutController', WorkoutController);
+*/
+
+// 3 anotacje inline
+/*
+angular.module('workout_seven').controller(['$scope', function($scope) {
+
+}]);
+*/
+
+// liczy się kolejność podania zależności, w takiej kolejności będą wstrzykiwane
+
+// 1
 angular.module('workout_seven').controller('WorkoutController', function($scope) {
 
 });
